7bd56f2d6d1dfa295cadc930f0de3569464da777
Andres MArquez <andrespipe012701@gmail.com>
2024-05-21 21:36:58 -0500

    Update main.js
    Creacion rama de prueba_andres con codigo JavaScript

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,9 @@
-console.log("Hola a todos en JavaScript")
+console.log("Hola a todos en JavaScript");
+
+let array_numbers = new Array();
+
+for (let index = 0; index <= 10; index++) {
+    array_numbers[index] = index;
+}
+
+console.log(array_numbers);
