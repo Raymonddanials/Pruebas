537b6c87a431cdad351317ab8b307366ebdcbbb8
Andres MArquez <andrespipe012701@gmail.com>
2024-05-22 20:55:31 -0500

    Update main.js
    Pruebas agregar elementos al DOM con JavaScript

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,37 @@
-console.log("Hola a todos en JavaScript");
+let nav_options = document.getElementById("nav_options");
 
-let array_numbers = new Array();
+let ul = document.createElement("ul");
+ul.classList.add("horizontal")
 
-for (let index = 0; index <= 10; index++) {
-    array_numbers[index] = index;
+for (let index = 1; index <= 3; index++) {
+    let li = document.createElement("li");
+    
+    let a = document.createElement("a");
+    a.href = "#";
+    a.innerText = "Opción " + index;
+
+    let img = document.createElement("img");
+    img.src = "../Img/_7th-March.jpg";
+    img.width = "20";
+    img.height = "20";
+
+    let subUl = document.createElement("ul");
+    subUl.classList.add("vertical");
+
+    for (let j = 1; j <= 3; j++) {
+        let subLi = document.createElement("li");
+
+        let subA = document.createElement("a");
+        subA.innerText = "Opción " + index + "." + j;
+
+        subLi.appendChild(subA);
+        subUl.appendChild(subLi);
+    }
+    
+    a.appendChild(img);
+    li.appendChild(a);
+    li.appendChild(subUl);
+    ul.appendChild(li);
 }
 
-console.log(array_numbers);
+nav_options.appendChild(ul);
